65284086081d74f3b94f449c22be03312235577e
5eqn <491100866@qq.com>
2023-07-03 09:22:20 +0800

    Add nginx for file

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+docker build . -t chart-nginx
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o bin/server
 cd bin
 docker build . -t chart-server
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 docker tag chart-server win:5000/chart-server
 docker push win:5000/chart-server
+docker tag chart-nginx win:5000/chart-nginx
+docker push win:5000/chart-nginx
